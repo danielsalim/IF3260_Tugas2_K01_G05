ff1ff2be0f0303905b5a6112e9109f622f47a683
hanseltanoto <13520046@std.stei.itb.ac.id>
2023-03-25 19:57:00 +0700

    fix: modify oblique angle

--- src/utils/util.js
+++ src/utils/util.js
@@ -51,7 +51,7 @@ function getProjectionMatrix(mode) {
             return [
                 1, 0, 0, 0,
                 0, 1, 0, 0,
-                Math.cos(64/180*Math.PI)/2, Math.cos(64/180*Math.PI)/2, 1, 0,
+                Math.cos(60/180*Math.PI)/2, Math.cos(60/180*Math.PI)/2, 1, 0,
                 0, 0, 0, 1
             ];
         case "perspective":
